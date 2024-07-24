53cb9c95ef0a542a34e9585be2b753ec4b45884b
Foster Setor <walkerdziwornu@gmail.com>
2024-07-24 13:30:54 +0000

    Loopsvar not_friendly in ES6

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+// Rewriting For_of_loops
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    newArray[idx] = appendString + value;
+  }
+
+  return newArray;
+}
