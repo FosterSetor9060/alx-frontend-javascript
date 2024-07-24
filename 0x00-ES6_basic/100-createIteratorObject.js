12c00dea997413e0e5e3c3b710b8ffa9a4db162e
Foster Setor <walkerdziwornu@gmail.com>
2024-07-24 13:47:19 +0000

    Iteratn reportObject_ES6

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+// interatn thru reportObjects
+export default function createIteratorObject(report) {
+  let result = [];
+  for (const value of Object.values(report.allEmployees)) {
+    result = [...result, ...value];
+  }
+
+  return result;
+}
